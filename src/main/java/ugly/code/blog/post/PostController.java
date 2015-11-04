cce400bda585189f597bbfa5e509b26c6a06ef5d
ethiaal <thiago.alves.de.moraes@ericsson.com>
2015-11-04 13:32:02 +0000

    Improving ugly test

--- src/main/java/ugly/code/blog/post/PostController.java
+++ src/main/java/ugly/code/blog/post/PostController.java
@@ -1,17 +1,16 @@
 package ugly.code.blog.post;
 
 import static java.lang.Long.parseLong;
-import static org.apache.http.impl.client.HttpClients.createDefault;
 
 import java.io.IOException;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.ParseException;
-import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 
 /**
@@ -24,6 +23,9 @@ public class PostController {
     @Inject
     HttpServletRequest request;
 
+    @Inject
+    HttpClient httpClient;
+
     public BlogPost createPost(final String message) throws ParseException, IOException {
         // Instantiate
         if (request.getSession() == null) {
@@ -33,10 +35,9 @@ public class PostController {
         post.setPost(message);
         post.setAuthor((User) request.getSession().getAttribute("user"));
         //Using Http Client
-        final CloseableHttpClient httpclient = createDefault();
         final HttpPost httpPost = new HttpPost("http://localhost:9009/db/data");
         //Sending post
-        final CloseableHttpResponse response = httpclient.execute(httpPost);
+        final HttpResponse response = httpClient.execute(httpPost);
         //Should be 200
         if (response.getStatusLine().getStatusCode() != 200) {
             throw new IllegalStateException("Something wrong happened! Call 0800 7070 7070123");
--- src/test/java/ugly/code/blog/post/PostControllerTest.java
+++ src/test/java/ugly/code/blog/post/PostControllerTest.java
@@ -11,12 +11,22 @@
  *----------------------------------------------------------------------------*/
 package ugly.code.blog.post;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicStatusLine;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -33,12 +43,26 @@ public class PostControllerTest {
     @Mock
     HttpSession session;
 
+    @Mock
+    HttpClient client;
+
+    @Mock
+    HttpResponse response;
+
     @InjectMocks
     PostController controller;
 
-    @Test(expected = IOException.class)
+    @Mock
+    HttpEntity entity;
+
+    @Test()
     public void test_PostController_createPost() throws ParseException, IOException {
-        Mockito.when(request.getSession()).thenReturn(session);
-        controller.createPost("What a nice day!");
+        when(request.getSession()).thenReturn(session);
+        when(client.execute(Mockito.any(HttpPost.class))).thenReturn(response);
+        when(response.getEntity()).thenReturn(entity);
+        when(response.getStatusLine()).thenReturn(new BasicStatusLine(HttpVersion.HTTP_1_1, 200, null));
+        when(entity.getContent()).thenReturn(new ByteArrayInputStream("4".getBytes()));
+        final BlogPost post = controller.createPost("What a nice day!");
+        assertTrue(post.getId().equals(4L));
     }
 }
